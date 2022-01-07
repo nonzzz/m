a24d24bef5eaae66dfac75d91011f3963af3e424
XeryYue <812137533@qq.com>
2022-01-07 15:19:53 +0800

    feat:add defineComponent typings

--- packages/wechat/src/component.ts
+++ packages/wechat/src/component.ts
@@ -1,3 +1,16 @@
 /**
  * define wechat component.
  */
+
+export type ComponentContext = WechatMiniprogram.Component.InstanceProperties &
+  Omit<
+    WechatMiniprogram.Component.InstanceMethods<Record<string, any>>,
+    'setData' | 'groupSetData' | 'hasBehavior'
+  >
+
+export type ComponentSetup<Props extends Record<string, any>> = (
+  props: Readonly<Props>,
+  context: ComponentContext
+) => void
+
+export function defineComponent(props, setup): void {}
