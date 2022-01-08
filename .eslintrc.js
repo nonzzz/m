2e4d9a3d957fa2881949c63a6bd48b4a500ab499
kanno <812137533@qq.com>
2022-01-08 15:04:18 +0800

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'kagura'],
+};
